b8f607bc9ca1df16e83ad088f91ff90465698f18
Mikhail Volodin <volodinmiisha1@gmail.com>
2025-04-24 20:07:37 +0300

    Improved errors_13. Improved error with adding item, and editing.

--- src/app/[locale]/item/[id]/page.tsx
+++ src/app/[locale]/item/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getItemById } from '@/lib/db/queries';
 import { Item } from '@/lib/interfaces';
 import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -12,19 +13,19 @@ export const metadata: Metadata = {
 export default async function ItemPage({ params }: { params: Promise<{ id: string }> }) {
   const id = (await params).id;
   const item: Item | null = await getItemById(Number(id));
+  const t = await getTranslations();
 
   if (!item) {
-    return <p className="p-4">Предмет не найден</p>;
+    return <p className="p-4">{t('notFound')}</p>;
   }
 
   return (
     <div className="p-4 flex flex-col gap-4">
       <p className="font-bold text-2xl">
-        Название:
-        <span className="ml-2 font-semibold text-xl">{item?.title}</span>
+        {t('nameOfTable')}:<span className="ml-2 font-semibold text-xl">{item?.title}</span>
       </p>
       <p className="font-bold text-2xl">
-        Описание:
+        {t('descriptionOfForm')}:
         <span className="ml-2 font-semibold text-xl">{item?.description}</span>
       </p>
       <Image
@@ -39,12 +40,18 @@ export default async function ItemPage({ params }: { params: Promise<{ id: strin
         href={`/item/${item.id}/edit`}
         className="inline-block mt-4 px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700"
       >
-        Редактировать
+        {t('edit')}
       </Link>
-      <p className="font-bold ">
-        Написать
-        <span className="ml-2 font-semibold text-xl">Ссылка на телеграм</span>
-      </p>
+      {item.telegramId ? (
+        <Link
+          href={`tg://user?id=${item.telegramId}`}
+          className="inline-block mt-4 px-4 py-2 text-white bg-green-600 rounded-lg hover:bg-green-700"
+        >
+          {t('telegramLink')}
+        </Link>
+      ) : (
+        <p className="text-gray-500">{t('noTelegramId')}</p>
+      )}
     </div>
   );
 }
