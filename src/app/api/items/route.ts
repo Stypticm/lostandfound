4476063d374c8ff1609bcba7189173084d77b408
Mikhail Volodin <volodinmiisha1@gmail.com>
2025-04-25 06:13:19 +0300

    Improved errors_17.

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -2,7 +2,14 @@ import { addItem } from '@/lib/db/queries';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
+  console.log('DATABASE_URL on Vercel:', process.env.DATABASE_URL);
+
   const body = await req.json();
   const item = await addItem(body);
-  return NextResponse.json(item);
+  return NextResponse.json(item, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
 }
