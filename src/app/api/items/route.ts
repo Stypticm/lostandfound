deb6ca386300fe88aa82534333317a786e1d2803
Mikhail Volodin <volodinmiisha1@gmail.com>
2025-06-16 14:59:13 +0300

    Update route.ts

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,6 +1,8 @@
 import { addItem } from '@/lib/db/queries';
 import { NextResponse, NextRequest } from 'next/server';
 
+console.log('PRISMA_DISABLE_PREPARED_STATEMENTS:', process.env.PRISMA_DISABLE_PREPARED_STATEMENTS);
+
 export async function POST(req: NextRequest) {
   try {
     console.log('DATABASE_URL on Vercel:', process.env.DATABASE_URL);
